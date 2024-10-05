bb4e01a1eea24b29ee412687ef608a4aa6fd5f35
Huajieyu001 <331173500@qq.com>
2024-10-05 23:53:02 +0800

    base:constant, method

---
+++ jvm/src/main/java/top/huajieyu001/jvm/method/MethodTest.java
@@ -0,0 +1,28 @@
+package top.huajieyu001.jvm.method;
+
+public class MethodTest {
+
+    public static int getI1(){
+        int i = 0;
+        i = i++;
+        return i;
+    }
+
+    public static int getI2(){
+        int i = 0;
+        i = ++i;
+        return i;
+    }
+
+    public static int getIJ(){
+        int i = 0;
+        int j = i + 1;
+        return j;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getI1());
+        System.out.println(getI2());
+        System.out.println(getIJ());
+    }
+}
