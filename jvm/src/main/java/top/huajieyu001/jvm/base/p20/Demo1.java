d2257bef939c9f7f77b34efed7a7ad3b6102c2b2
Huajieyu001 <331173500@qq.com>
2024-10-08 23:52:51 +0800

    1008

--- jvm/src/main/java/top/huajieyu001/jvm/base/p20/Demo1.java
+++ jvm/src/main/java/top/huajieyu001/jvm/base/p20/Demo1.java
@@ -2,24 +2,47 @@ package top.huajieyu001.jvm.base.p20;
 
 public class Demo1 {
 
-    public static void test1(){
-        int i = 0;
-        int j = 1;
-    }
-    public void test2(){
+    public static void test1() {
         int i = 0;
         int j = 1;
     }
 
-    public void test3(int k, int m){
+    public void test2() {
         int i = 0;
         int j = 1;
     }
 
-    public void test4(int k, int m){
+    public void test3(int k, int m) {
+        int i = 0;
+        int j = 1;
+    }
+
+    public void test4(int k, int m) {
         {
             int i = 0;
             int j = 1;
+        }
+    }
+
+    public void test5(int l, int m) {
+        {
+            int i = 0;
+            int j = 1;
+        }
+        {
+            int k = 0;
+        }
+        int a = 0;
+        long b = 1;
+    }
+
+    public void test6() {
+        int i = 0;
+        try{
+            i = 1;
+        }
+        catch (Exception e){
+            i = 2;
         }
     }
 
