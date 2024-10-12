c4b3265e346b17399941bcac95f96b1bb5f723b6
Huajieyu001 <331173500@qq.com>
2024-10-12 23:40:14 +0800

    1012

---
+++ jvm/src/main/java/top/huajieyu001/jvm/base/p26/StringConst.java
@@ -0,0 +1,12 @@
+package top.huajieyu001.jvm.base.p26;
+
+public class StringConst {
+
+    public static void main(String[] args) {
+        String a = "1";
+        String b = "2";
+        String c = "12";
+        String d = a + b;
+        System.out.println(c == d);
+    }
+}
---
+++ jvm/src/main/java/top/huajieyu001/jvm/base/p26/StringConst2.java
@@ -0,0 +1,12 @@
+package top.huajieyu001.jvm.base.p26;
+
+public class StringConst2 {
+
+    public static void main(String[] args) {
+        String a = "1";
+        String b = "2";
+        String c = "12";
+        String d = "1" + "2";
+        System.out.println(c == d);
+    }
+}
